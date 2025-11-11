e84f0b7d207f63928820f6c404a3bf024c65b109
Anand Thakur <satul95060@gmail.com>
2025-11-11 22:53:42 +0530

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,5 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { CircleCheckBig, ClipboardClock, ClipboardCheck, Trash2, Users } from "lucide-react";
 import { MdDashboard } from "react-icons/md";
-
 import {
   Sidebar,
   SidebarProvider,
@@ -8,63 +7,56 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarHeader,
-} from "@/components/ui/sidebar"
+  SidebarFooter
+} from "@/components/ui/sidebar";
 
-// Menu items.
+
+
+// Menu items
 const items = [
-  {
-    title: "Home",
-    url: "./Navbar.jsx",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
+  { title: "Complete", url: "/complete", icon: CircleCheckBig },
+  { title: "In Process", url: "/in-process", icon: ClipboardClock },
+  { title: "To Do", url: "/todo", icon: ClipboardCheck },
+  { title: "Team", url: "/team", icon: Users },
+  { title: "Trash", url: "/trash", icon: Trash2 },
 ];
 
-export function  AppSidebar() {
+export function AppSidebar() {
   
-        return (
-            <SidebarProvider>
-          <Sidebar className= "flex left-0 right-0  top-15">
-            <SidebarHeader className= " flex flex-row items-center text-2xl text-blue-500 gap-2" >
-            <MdDashboard />
-              <p className="font-bold">Task Manager</p>
-            </SidebarHeader>
-                  <SidebarMenu>
-                    {items.map((item) => (
-                      <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton asChild>
-                          <a href={item.url}>
-                            <item.icon />
-                            <span>{item.title}</span>
-                          </a>
-                        </SidebarMenuButton>
-                      </SidebarMenuItem>
-                    ))}
-                  </SidebarMenu>
-          </Sidebar>
-          </SidebarProvider>
-        )
-      }
+
+  return (
+    <SidebarProvider>
+      <Sidebar className="flex left-0 right-0 top-15">
+        <SidebarHeader className="flex flex-row items-center text-2xl text-blue-500 gap-2">
+          <MdDashboard />
+          <p className="font-bold">Dashboard</p>
+        </SidebarHeader>
+
+        <SidebarMenu className="gap-1.5">
+          {items.map((item) => {
+
+        
+
+            return (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  asChild
+                >
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+                
+              </SidebarMenuItem>
+            );
+          })}
+        </SidebarMenu>
       
+      </Sidebar>
+    </SidebarProvider>
+   
+  );
+}
 
-
-export default AppSidebar
+export default AppSidebar;
