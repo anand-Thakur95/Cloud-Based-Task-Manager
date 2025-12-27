994470132901c2310d2e0f3b959ec57deb07ed59
Anand Thakur <satul95060@gmail.com>
2025-12-27 23:58:36 +0530

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+
 import { Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom';
 
 import Login from './pages/Login';
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaList } from 'react-icons/fa'
+import { MdGridView } from 'react-icons/md'
+import Loading from '../components/Loading';
 
+
+
+const TABS = [
+  {title:"Board View", icon:<MdGridView/>},
+  {title: "List View", icon: <FaList/>}
+];
+
+const TASK_TYPE = {
+  todo: "bg-blue-600",
+  "in process": "bg-yellow-600",
+  comleted: "bg-green-600",
+};
 function Tasks() {
-  return (
-    <div>
-      
-    </div>
+  const params = useParams();
+
+  const [selected, setSelected] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(true)
+
+
+  return loading ? (
+  <div className='py-10'>
+    <Loading/>
+  </div>
+  ) : (
+    <div></div>
   )
 }
 
