2d53dada4d9f4ad82b68dcfc930e22cd968daef6
Anand Thakur <satul95060@gmail.com>
2025-12-30 21:38:31 +0530

    task

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -18,35 +18,58 @@ const TASK_TYPE = {
   "in process": "bg-yellow-600",
   comleted: "bg-green-600",
 };
-function Tasks() {
+const Tasks = () => {
   const params = useParams();
 
   const [selected, setSelected] = useState(0);
   const [open, setOpen] = useState(false);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false);
 
-const status = params?.status || "";
+  const status = params?.status || "";
+
   return loading ? (
-  <div className='py-20 '>
-    <Loading/>
-  </div>
+    <div className='py-10'>
+      <Loading />
+    </div>
   ) : (
     <div className='w-full'>
-      <div className='flex items-center justify-center mb-4'>
-        <Title title={status ? `${status} Tasks` : "Task"} />
+      <div className='flex items-center justify-between mb-4'>
+        <Title title={status ? `${status} Tasks` : "Tasks"} />
 
-
-        {
-          !status && <Button
-          label="Create Task"
-          icon={<IoMdAdd className="text-lg" />}
-          className="flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5" />
-        }
+        {!status && (
+          <Button
+            onClick={() => setOpen(true)}
+            label='Create Task'
+            icon={<IoMdAdd className='text-lg' />}
+            className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
+          />
+        )}
       </div>
-      
+
+      <Tabs tabs={TABS} setSelected={setSelected}>
+        {!status && (
+          <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
+            <TaskTitle label='To Do' className={TASK_TYPE.todo} />
+            <TaskTitle
+              label='In Progress'
+              className={TASK_TYPE["in progress"]}
+            />
+            <TaskTitle label='completed' className={TASK_TYPE.completed} />
+          </div>
+        )}
+
+        {selected !== 1 ? (
+          <BoardView tasks={tasks} />
+        ) : (
+          <div className='w-full'>
+            <Table tasks={tasks} />
+          </div>
+        )}
+      </Tabs>
+
+      <AddTask open={open} setOpen={setOpen} />
     </div>
-  )
+  );
+};
 
-}
-
-export default Tasks
+export default Tasks;
