5c87c0a2954b29a5be087b1e664258731417323d
Anand Thakur <satul95060@gmail.com>
2026-01-18 12:46:20 +0530

    Remove Bankend folder

--- Backend/route/User.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { Register } from "../controllers/user.controller.js"
-
-const UserRouter = express.Router()
-
-UserRouter.post('/register', Register)
-
-
-export default UserRouter
